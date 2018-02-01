0c3e256dffdd9e37a99dd5b80986cf98cc098eeb
Amrykid <Amrykid@gmail.com>
2018-02-01 12:16:34 -0600

    Added dashboard, login, register.

--- app.py
+++ app.py
@@ -9,10 +9,21 @@ def index():
     #return 'Hello, World!'
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 @app.errorhandler(404)
 def page_not_found(error):
     return "We  didn't think it be like it was but it do. We can't find that page!", 404
-
 
 if __name__ == "__main__":
     app.run()

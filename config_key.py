d33497f76f1b7161fa1e21e42743e5e8aa1f771a
kskenandore <keiraskenandore@gmail.com>
2018-02-03 15:44:33 -0600

    Added HTML5 to base, improved header styles, added content

--- config_key.py
+++ config_key.py
@@ -1,6 +1,6 @@
 import sys
 def configureFlaskSecretKey(app):
-    app.secret_key = '' #set this key before doing anything!
+    app.secret_key = '\xf5m\xa8\xc5\x95)\x14\xdd\x0cV\xe4c.\xfb\xde\xaa(a\x04j\x92\xde\x99M' #set this key before doing anything!
     # see http://flask.pocoo.org/docs/0.12/quickstart/#sessions
     if app.secret_key == '': 
         #try to force the server to stop
